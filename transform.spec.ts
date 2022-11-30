634cd9354771366eafdae850bc676674567a77eb
lil climate <lilclimate@gmail.com>
2022-11-30 23:33:20 +0800

    feat: 转换新ast树，定义测试用例

---
+++ transform.spec.ts
@@ -0,0 +1,134 @@
+import { test, expect } from 'vitest'
+import { NodeTypes, RootNode } from './ast';
+test.skip("transformer", () => {
+  const originalAST: RootNode = {
+    type: NodeTypes.Program,
+    body: [
+      {
+        type: NodeTypes.CallExpression,
+        name: "add",
+        params: [
+          {
+            type: NodeTypes.NumberLiteral,
+            value: 2,
+          },
+          {
+            type: NodeTypes.CallExpression,
+            name: "subtract",
+            params: [
+              {
+                type: NodeTypes.NumberLiteral,
+                value: 4,
+              },
+              {
+                type: NodeTypes.NumberLiteral,
+                value: 2,
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  };
+
+  const transformedAST = {
+    type: NodeTypes.Program,
+    body: [
+      {
+        type: "ExpressionStatement",
+        expression: {
+          type: "CallExpression",
+          callee: {
+            type: "Identifier",
+            name: "add",
+          },
+          arguments: [
+            {
+              type: "NumberLiteral",
+              value: "2",
+            },
+            {
+              type: "CallExpression",
+              callee: {
+                type: "Identifier",
+                name: "subtract",
+              },
+              arguments: [
+                {
+                  type: "NumberLiteral",
+                  value: "4",
+                },
+                {
+                  type: "NumberLiteral",
+                  value: "2",
+                },
+              ],
+            },
+          ],
+        },
+      },
+    ],
+  };
+
+  expect(transformer(originalAST)).toEqual(transformedAST);
+});
+
+test.skip("NumberLiteral", () => {
+  const originalAST:RootNode = {
+    type: NodeTypes.Program,
+    body: [
+      {
+        type: NodeTypes.NumberLiteral,
+        value: 2,
+      },
+    ],
+  };
+
+  const transformedAST = {
+    type: "Program",
+    body: [
+      {
+        type: "ExpressionStatement",
+        expression: {
+          type: "NumberLiteral",
+          value: "2",
+        },
+      },
+    ],
+  };
+  expect(transformer(originalAST)).toEqual(transformedAST);
+});
+
+test.skip("callExpresstion add();", () => {
+  const originalAST:RootNode = {
+    type: NodeTypes.Program,
+    body: [
+      {
+        type: NodeTypes.CallExpression,
+        name: "add",
+        params: [],
+      },
+    ],
+  };
+
+  const transformedAST = {
+    type: "Program",
+    body: [
+      {
+        type: "ExpressionStatement",
+        expression: {
+          type: "CallExpression",
+          callee: {
+            type: "Identifier",
+            name: "add",
+          },
+          arguments: [],
+        },
+      },
+    ],
+  };
+  expect(transformer(originalAST)).toEqual(transformedAST);
+});
+
+function transformer(originalAST: RootNode): any {
+}
