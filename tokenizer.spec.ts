b4d5ec7ed3861f05e5dc7496b0ec3cf1dbe502d2
lil climate <lilclimate@gmail.com>
2022-11-30 10:48:27 +0800

    feat: 定义词法解析测试用例

---
+++ tokenizer.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from 'vitest'
+test.skip('tokenizer', () => { 
+	const code = `(add 2 (subtract 4 2))`;
+	const tokens = 
+				[
+						{ type: TokenTypes.Paren,  value: '('        },
+						{ type: TokenTypes.Name,   value: 'add'      },
+						{ type: TokenTypes.Number, value: '2'        },
+						{ type: TokenTypes.Paren,  value: '('        },
+						{ type: TokenTypes.Name,   value: 'subtract' },
+						{ type: TokenTypes.Number, value: '4'        },
+						{ type: TokenTypes.Number, value: '2'        },
+						{ type: TokenTypes.Paren,  value: ')'        },
+						{ type: TokenTypes.Paren,  value: ')'        },
+				];
+	expect(tokenizer(code)).toEqual(tokens);
+})
+
+enum TokenTypes { 
+	Paren,
+	Name,
+	Number,
+}
+
+function tokenizer(code: string): any {
+}
+
