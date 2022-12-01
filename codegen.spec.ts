f66361bca4e36840ae90389afa76cb251de2feab
lil climate <lilclimate@gmail.com>
2022-12-01 11:00:24 +0800

    feat: 定义ast生成代码测试用例

---
+++ codegen.spec.ts
@@ -0,0 +1,143 @@
+import { test, expect } from "vitest";
+import { NodeTypes } from "./ast";
+test.skip("two ExpressionStatement", () => {
+  const ast = {
+    type: NodeTypes.Program,
+    body: [
+      {
+        type: "ExpressionStatement",
+        expression: {
+          type: "CallExpression",
+          callee: {
+            type: "Identifier",
+            name: "add",
+          },
+          arguments: [
+            {
+              type: "NumberLiteral",
+              value: "2",
+            },
+            {
+              type: "CallExpression",
+              callee: {
+                type: "Identifier",
+                name: "subtract",
+              },
+              arguments: [
+                {
+                  type: "NumberLiteral",
+                  value: "4",
+                },
+                {
+                  type: "NumberLiteral",
+                  value: "2",
+                },
+              ],
+            },
+          ],
+        },
+      },
+      {
+        type: "ExpressionStatement",
+        expression: {
+          type: "CallExpression",
+          callee: {
+            type: "Identifier",
+            name: "add",
+          },
+          arguments: [
+            {
+              type: "NumberLiteral",
+              value: "2",
+            },
+            {
+              type: "CallExpression",
+              callee: {
+                type: "Identifier",
+                name: "subtract",
+              },
+              arguments: [
+                {
+                  type: "NumberLiteral",
+                  value: "4",
+                },
+                {
+                  type: "NumberLiteral",
+                  value: "2",
+                },
+              ],
+            },
+          ],
+        },
+      },
+    ],
+  };
+
+  expect(codegen(ast)).toMatchInlineSnapshot(
+    '"add(2, subtract(4, 2));add(2, subtract(4, 2));"'
+  );
+});
+
+test.skip("codegen", () => {
+  const ast = {
+    type: NodeTypes.Program,
+    body: [
+      {
+        type: "ExpressionStatement",
+        expression: {
+          type: "CallExpression",
+          callee: {
+            type: "Identifier",
+            name: "add",
+          },
+          arguments: [
+            {
+              type: "NumberLiteral",
+              value: "2",
+            },
+            {
+              type: "CallExpression",
+              callee: {
+                type: "Identifier",
+                name: "subtract",
+              },
+              arguments: [
+                {
+                  type: "NumberLiteral",
+                  value: "4",
+                },
+                {
+                  type: "NumberLiteral",
+                  value: "2",
+                },
+              ],
+            },
+          ],
+        },
+      },
+    ],
+  };
+
+  expect(codegen(ast)).toMatchInlineSnapshot('"add(2, subtract(4, 2));"');
+});
+
+
+test.skip('NumberLiteral', () => { 
+	const ast = {
+    type: NodeTypes.Program,
+    body: [
+      {
+        type: "ExpressionStatement",
+        expression: {
+          type: "NumberLiteral",
+          value: "2",
+        },
+      },
+    ],
+  };
+
+  expect(codegen(ast)).toMatchInlineSnapshot('"2;"');
+});
+
+function codegen(node): any {
+}
